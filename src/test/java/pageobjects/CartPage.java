93b09c8654af2c42dbd3e7c8a5f21197fe4b27b8
Alexandr Drankevich <a.a.drankevich@gmail.com>
2022-05-10 22:21:06 +0300

    refactor code

--- src/test/java/pageobjects/CartPage.java
+++ src/test/java/pageobjects/CartPage.java
@@ -1,5 +1,7 @@
 package pageobjects;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -9,6 +11,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class CartPage extends AbstractPage {
+    private static final Logger logger = LogManager.getLogger();
     private String cartElementsLocator = "//section[@data-testid='cart__list']//h3";
 
     public CartPage(WebDriver driver) {
@@ -20,6 +23,7 @@ public class CartPage extends AbstractPage {
         waitForVisibilityOfElement(driver.findElement(By.xpath(cartElementsLocator)));
         List<WebElement> elements = driver.findElements(By.xpath(cartElementsLocator));
         List<String> productCollect = elements.stream().map(WebElement::getText).collect(Collectors.toList());
+        logger.info(productCollect);
         return productCollect.contains(productName);
     }
 }
