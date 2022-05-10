b22e38d53d21d4dcf41f0b2a46ad187fbb66e342
Alexandr Drankevich <a.a.drankevich@gmail.com>
2022-05-10 19:13:52 +0300

    refactor code

--- src/test/java/test/DodopizzaTest.java
+++ src/test/java/test/DodopizzaTest.java
@@ -12,7 +12,7 @@ public class DodopizzaTest extends AbstractTest {
     private final String drinkName = "Pepsi";
 
     @Test
-    public void testAddPizzaToBucket() {
+    public void testAddPizzaToCart() {
         HomePageForYourCity homePageForYourCity = new HomePage(driver)
                 .openPage().chooseCity(cityName)
                 .chooseProductAddToCart(pizzaName)
